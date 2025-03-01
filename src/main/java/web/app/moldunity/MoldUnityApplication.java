69997d971d3131e5c6678e3a1ca9ede991d2d83a
JSanea <josualexandru10@gmail.com>
2025-03-01 15:00:53 +0200

    Start

---
+++ src/main/java/web/app/moldunity/MoldUnityApplication.java
@@ -0,0 +1,13 @@
+package web.app.moldunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoldUnityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoldUnityApplication.class, args);
+	}
+
+}
